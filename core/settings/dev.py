de2b0513b6ea76234413681c5a11ee7551dc1b5c
shadikhasan <shadik.sk420@gmail.com>
2025-11-05 19:52:04 +0600

    fix: Update environment settings for development; change DJANGO_SETTINGS_MODULE to dev and correct database password variable

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -1,6 +1,8 @@
 # D E V E L O P M E N T    S E T T I N G S
 # core/settings/dev.py
 from .base import *
+from dotenv import load_dotenv
+load_dotenv()
 
 DEBUG = True
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
@@ -10,7 +12,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
